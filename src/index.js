9ca0b1da6ba73fdd57989cf60ea65e0c834b2934
Vatican Memes <feehanharry@gmail.com>
2024-01-08 17:43:48 +1100

    webpack workinggit add .!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-para = document.createElement("p")
-para.innerText = "hello"
-document.getElementById("content").appendChild(para)
+import _ from 'lodash';
+
+function createPara(text) {
+    const para = document.createElement("p")
+    para.innerText = text
+    return para
+}
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(createPara("Webpack is working!!!"));
