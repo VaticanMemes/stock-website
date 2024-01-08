ee9364c593a37e5d68e2bad64ffae72a1f123a3c
Vatican Memes <feehanharry@gmail.com>
2024-01-08 19:24:22 +1100

    website = working

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Devlopment',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
