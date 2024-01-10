5768169c561b69f40a055475ea32670c0954e7bd
Vatican Memes <feehanharry@gmail.com>
2024-01-10 22:35:09 +1100

    allowed for lower case submissions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 
 const API_key = "wgOwUik_ijPDrNSCYdgpwnNMy59pFJ99"
-const tickerAndPrice = []
 
 async function fetchStock(apiKey, stockticker) {
 	let API_call = `https://api.polygon.io/v2/aggs/ticker/${stockticker}/prev?adjusted=true&apiKey=${apiKey}`
@@ -34,5 +33,5 @@ function stockPriceDisplayer(input) {
 
 document.getElementById("submit").addEventListener("click", (evt) => {
 	evt.preventDefault();
-	fetchStock(API_key, document.getElementById("ticker_input").value);
+	fetchStock(API_key, (document.getElementById("ticker_input").value).toUpperCase());
 	})
